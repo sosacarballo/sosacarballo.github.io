93634801dd7d24b5169ae18031ab4b20034d2b30
sosacarballo <122845774+sosacarballo@users.noreply.github.com>
2023-03-03 22:28:24 -0300

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,36 @@
+document.getElementById("idLista1").style.display = "none"
+document.getElementById("idLista2").style.display = "none"
+document.getElementById("idLista3").style.display = "none"
+
+function MyFunction1() {
+    var x = document.getElementById("idLista1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function MyFunction2() {
+    var x = document.getElementById("idLista2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function MyFunction3() {
+    var x = document.getElementById("idLista3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function MyFunction4() {
+    var x = document.getElementById("idLista4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
